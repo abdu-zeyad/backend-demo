2c8c674ead76443db4a01b9298d19b879bb313db
abdu-zeyad <abd.d@live.com>
2021-07-05 15:41:07 +0300

    finished

--- server.js
+++ server.js
@@ -2,17 +2,29 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { default: axios } = require("axios");
-// const axios = require("axios");
-const server = express();
-server.use(cors());
-
+const axios = require("axios");
+const app = express();
+app.use(cors());
 const PORT = process.env.PORT;
+app.use(express.json());
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/cars", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//schema
+const carSchema = new mongoose.Schema({
+  name: String,
+  avg_price: String,
+});
+const Car = mongoose.model("car", carSchema);
 
-//
 // routes
-server.get("/all", allDataHandler);
-
+app.get("/all", allDataHandler);
+app.post("/addToFav", addToFavHandler);
+app.get("/getFav", getFavData);
+app.delete("/delete", deletefav);
+app.put("/update", updatehandler);
 // handlers
 function allDataHandler(req, res) {
   const url =
@@ -22,14 +34,50 @@ function allDataHandler(req, res) {
   });
 }
 
+function addToFavHandler(req, res) {
+  console.log(req.body);
+  const { name, avg_price } = req.body;
+  console.log(name);
+  const item = new Car({ name: name, avg_price: avg_price });
+  item.save();
+}
+
+function getFavData(req, res) {
+  Car.find({}, (err, data) => {
+    res.send(data);
+  });
+}
+
+function deletefav(req, res) {
+  const id = req.query.id;
+  console.log(id);
+  Car.deleteOne({ _id: id }, (err, data) => {
+    Car.find({}, (err, data) => {
+      res.send(data);
+    });
+  });
+}
+function updatehandler(req, res) {
+  const { name, avg_price, id } = req.body;
+
+  Car.find({ _id: id }, (err, data) => {
+    data[0].name = name;
+    data[0].avg_price = avg_price;
+    data[0].save().then(() => {
+      Car.find({}, (err, data) => {
+        res.send(data);
+      });
+    });
+  });
+}
 //
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("hi");
 });
-server.get("*", (req, res) => {
+app.get("*", (req, res) => {
   res.send("Not found");
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening to PORT ${PORT}`);
 });
